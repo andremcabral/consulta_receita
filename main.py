b77c6c09fc00a5a63581e7dbcebc9b1df196a078
Andre <andremcabral@gmail.com>
2025-08-23 19:24:30 -0300

    importando dados da planilha

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from consultaReceita import verifica_receita
 from anexaArquivo import anexar_arquivo
 from selenium import webdriver
@@ -6,30 +7,21 @@ chrome_options = Options()
 chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("start-maximized")
 chrome_options.add_argument("--disable-extensions")
+nome_arquivo = 'dados.xlsx'
 
-mutuarios = [
-    {
-    'CPF' : '00408731702'
-    ,'DATA_NASC' : '11061968'
-    ,'NUMERO_SEI' : 'SEI-490002/001152/2025'
-    ,'NOME' : 'RECEITA_IVONE'
-    ,'COMPROVANTE' : ""
-    }
-    ,{
-    'CPF' : '00408731702'
-    ,'DATA_NASC' : '11061968'
-    ,'NUMERO_SEI' : 'SEI-490002/001152/2025'
-    ,'NOME' : 'SEGUNDA_VEZ'
-    ,'COMPROVANTE' : ""
-    }
-]
+df = pd.read_excel("dados.xlsx", dtype=str)
+df = df.fillna('')
+dados = df.to_dict(orient="records")
+print(dados)
 
-print(mutuarios)
-with webdriver.Chrome(options=chrome_options) as navegador_receita:
-    for index, mutuario in enumerate(mutuarios):
-        mutuarios[index]['COMPROVANTE'] = verifica_receita(mutuario, navegador_receita)
-print(mutuarios)
-
-with webdriver.Chrome(options=chrome_options) as navegador_sei:
-    for index, mutuario in enumerate(mutuarios):
-        anexar_arquivo(mutuario, navegador_sei)
+def testar():
+    with webdriver.Chrome(options=chrome_options) as navegador_receita:
+        for index, mutuario in enumerate(dados):
+            dados[index]['COMPROVANTE'] = verifica_receita(mutuario, navegador_receita)
+    print(dados)
+    # with webdriver.Chrome(options=chrome_options) as navegador_sei:
+    #     for index, mutuario in enumerate(mutuarios):
+    #         anexar_arquivo(mutuario, navegador_sei)
+testar()
+df_atualizado = pd.DataFrame(dados)
+df_atualizado.to_excel("dados.xlsx", index=False)
